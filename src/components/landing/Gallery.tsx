6d02455091ef3a2cabeca7838ecd7d4bc724d720
melvsalonga <melvinsalonga2999@gmail.com>
2025-12-26 19:01:17 +0800

    refactor: add portrait image to Gallery component for enhanced visual variety

--- src/components/landing/Gallery.tsx
+++ src/components/landing/Gallery.tsx
@@ -14,7 +14,11 @@ const galleryImages = [
     alt: "Abstract Art",
     span: "row-span-1"
   },
- 
+  {
+    src: "/Portrait.png",
+    alt: "Portrait",
+    span: "row-span-1"
+  },
   {
     src: "/Landscape.png",
     alt: "Landscape",
